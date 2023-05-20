1ad90ef2ffcc1f1b04da2ccc04f466214a7a0ba2
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-05-20 20:23:06 +0530

    Create switch_for_while.cs

---
+++ switch_for_while.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Switch
+
+    {
+        static void Main(string[] args)
+        {
+            /*var season = Season.Autumn;
+            var name = "John Smith";
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn and a beautiful season.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's perfect time to go to beach.");
+                    break;
+
+                default: 
+                    Console.WriteLine("I don't know what is the season!");
+                    break;*/
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if(String.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                Console.WriteLine("@Echo: " + input);
+            }
+
+                   
+            }
+        }
+    }
+
